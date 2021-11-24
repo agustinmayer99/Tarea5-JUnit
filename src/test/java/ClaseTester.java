2db9f00aff91e664468d06dffc0e680ee4df231c
mayeragustin99 <mayeragustin99@gmail.com>
2021-11-24 15:44:16 -0300

    borrar las que fallan

--- src/test/java/ClaseTester.java
+++ src/test/java/ClaseTester.java
@@ -14,32 +14,14 @@ public class ClaseTester {
     }
 
     @Test
-    public void esImparTestFalla(){
-        boolean resultado = CAT.esImpar(4);
-        assertTrue(resultado);
-    }
-
-    @Test
     public void cuadradoDelNumeroTest(){
         int resultado = CAT.cuadradoDelNumero(3);
         assertEquals(9,resultado);
     }
 
     @Test
-    public void cuadradoDelNumeroTestFalla(){
-        int resultado = CAT.cuadradoDelNumero(3);
-        assertEquals(10,resultado);
-    }
-
-    @Test
     public void longitudStringTest(){
         int resultado = CAT.longitudString("Testing");
-        assertEquals(7,resultado);
-    }
-
-    @Test
-    public void longitudStringTestFalla(){
-        int resultado = CAT.longitudString("Ingenieria");
         assertEquals(7,resultado);
     }
 }
